36134f81731311fe7b413a787f8889368627ffc6
theKuanWay <liewkw94@gmail.com>
2024-10-17 20:37:48 +0800

    make a firebase.js to put initialisation

--- src/index.js
+++ src/index.js
@@ -6,16 +6,6 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyBFg45eYXHPD0wJWTpyMagdTLFVOVeA3p0",
-  authDomain: "project-react-sctp-grp1.firebaseapp.com",
-  projectId: "project-react-sctp-grp1",
-  storageBucket: "project-react-sctp-grp1.appspot.com",
-  messagingSenderId: "511748686532",
-  appId: "1:511748686532:web:60fc20c2e0636f684b3f91",
-};
-
 root.render(
   <GoogleOAuthProvider clientId="435750358779-n8spd0p14gt95egoscq9afjqno8gqlqk.apps.googleusercontent.com">
     <React.StrictMode>
